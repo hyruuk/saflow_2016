f109018125f25c6a664e7b18cf356714906550de
hyruuk lab <yharel109@gmail.com>
2019-09-13 01:57:15 -0400

    finally debugged plot_properties

--- scripts/saflow_preproc.py
+++ scripts/saflow_preproc.py
@@ -10,18 +10,18 @@ import os
 import time
 
 
-filepath = '/home/hyruuk/data_temp/inscapesMEG/P03_NEUROMOD-jboyle_20190529_01.ds'
+filepath = '/home/karim/DATA/DATAmeg_gradCPT/20190820/SA11_SAflow-yharel_20190820_01.ds'
 from matplotlib.backends.backend_pdf import PdfPages
 
 with PdfPages('multipage_pdf.pdf') as pdf:
     raw_data = read_raw_ctf(filepath, preload=True)
     picks = mne.pick_types(raw_data.info, meg=True, eog=True, exclude='bads')
 
-    fig = raw_data.plot();
+    fig = raw_data.plot(show=False);
     pdf.savefig(fig)
     closefig(fig)
 
-    fig = raw_data.plot_psd(average=False, picks=picks);
+    fig = raw_data.plot_psd(average=False, picks=picks, show=False);
     pdf.savefig(fig)
     closefig(fig)
 
@@ -32,13 +32,13 @@ with PdfPages('multipage_pdf.pdf') as pdf:
     raw_data.filter(low_cutoff, high_cutoff, fir_design="firwin")
     raw_data.notch_filter(np.arange(60, high_cutoff+1, 60), picks=picks, filter_length='auto',phase='zero', fir_design="firwin")
 
-    fig = raw_data.plot_psd(average=False, picks=picks);
+    fig = raw_data.plot_psd(average=False, picks=picks, show=False);
     pdf.savefig(fig)
     closefig(fig)
 
     ## ICA
     ica = ICA(n_components=20, random_state=0).fit(raw_data, decim=3)
-    fig = ica.plot_sources(raw_data);
+    fig = ica.plot_sources(raw_data, show=False);
     pdf.savefig(fig)
     closefig(fig)
     fmax = 40. ## correlation threshold for ICA components (maybe increase to 40. ?)
@@ -47,24 +47,29 @@ with PdfPages('multipage_pdf.pdf') as pdf:
     ecg_epochs = create_ecg_epochs(raw_data, ch_name='EEG059')
     ecg_inds, ecg_scores = ica.find_bads_ecg(ecg_epochs, ch_name='EEG059')
 
-    fig = ica.plot_scores(ecg_scores, ecg_inds);
+    fig = ica.plot_scores(ecg_scores, ecg_inds, show=False);
     pdf.savefig(fig)
     closefig(fig)
 
-    fig = ica.plot_properties(ecg_epochs, picks=ecg_inds, psd_args={'fmax': fmax}, image_args={'sigma': 1.});
-    pdf.savefig(fig)
-    closefig(fig)
+    fig = list()
+    fig = ica.plot_properties(ecg_epochs, picks=ecg_inds, psd_args={'fmax': fmax}, image_args={'sigma': 1.}, show=False);
+    for figure in fig:
+        pdf.savefig(figure)
+        closefig(figure)
+
 
     ## FIND EOG COMPONENTS
     eog_epochs = create_eog_epochs(raw_data, ch_name='EEG057')
     eog_inds, eog_scores = ica.find_bads_eog(eog_epochs, ch_name='EEG057')
-    fig = ica.plot_scores(eog_scores, eog_inds);
+    fig = ica.plot_scores(eog_scores, eog_inds, show=False);
     pdf.savefig(fig)
     closefig(fig)
 
-    fig = ica.plot_properties(eog_epochs, picks=eog_inds, psd_args={'fmax': fmax}, image_args={'sigma': 1.});
-    pdf.savefig(fig)
-    closefig(fig)
+    fig = list()
+    fig = ica.plot_properties(eog_epochs, picks=eog_inds, psd_args={'fmax': fmax}, image_args={'sigma': 1.}, show=False);
+    for figure in fig:
+        pdf.savefig(figure)
+        closefig(figure)
 
     ## EXCLUDE COMPONENTS
     ica.exclude = ecg_inds
@@ -72,10 +77,10 @@ with PdfPages('multipage_pdf.pdf') as pdf:
     ica.exclude = eog_inds
     ica.apply(raw_data)
 
-    fig = raw_data.plot(); # Plot the clean signal.
+    fig = raw_data.plot(show=False); # Plot the clean signal.
     pdf.savefig(fig)
     closefig(fig)
 
     ## SAVE PREPROCESSED FILE
     time.sleep(30)
-    raw_data.save(filepath + '_preprocessed.fif.gz', overwrite=True)
+    raw_data.save(filepath + '_preprocessed_raw.fif.gz', overwrite=True)

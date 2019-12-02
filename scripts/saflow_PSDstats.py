88f42536772820d7ffd922ae90199a024c1d546c
hyruuk lab <yharel109@gmail.com>
2019-12-02 13:15:05 -0500

    started rearranging scripts to go toward fully BIDS-compatible pipeline

--- scripts/saflow_PSDstats.py
+++ scripts/saflow_PSDstats.py
@@ -1,4 +1,4 @@
-from saflow_utils import array_topoplot, create_pval_mask
+from saflow_utils import array_topoplot, create_pval_mask, load_PSD_data
 from scipy.io import loadmat
 import mne
 from hytools.meg_utils import get_ch_pos
@@ -10,12 +10,8 @@ from mlneurotools.stats import ttest_perm
 FOLDERPATH = '/storage/Yann/saflow_DATA/'
 PSDS_DIR = FOLDERPATH + 'saflow_PSD/'
 IMG_DIR = '/home/karim/pCloudDrive/science/saflow/images/'
-
-
 FREQS_NAMES = ['theta', 'alpha', 'lobeta', 'hibeta', 'gamma1', 'gamma2', 'gamma3']
 SUBJ_LIST = ['04', '05', '06', '07', '08', '09', '10', '11', '12', '13']
-#SUBJ_LIST = ['14']
-#BLOCS_LIST = ['5', '6']
 BLOCS_LIST = ['1','2','3', '4', '5', '6']
 
 
@@ -26,51 +22,37 @@ if __name__ == "__main__":
     ch_file = '/storage/Yann/saflow_DATA/alldata/SA04_SAflow-yharel_20190411_01.ds'
     raw = mne.io.read_raw_ctf(ch_file)
     ch_xy = get_ch_pos(raw)
-    toplot = []
+    PSD_alldata = load_PSD_data(SUBJ_LIST, BLOCS_LIST, FREQS_NAMES, PSDS_DIR)
+    power_diff = []
+    masks = []
     tvalues = []
     pvalues = []
-    masks = []
-    for i, freq_name in enumerate(FREQS_NAMES):
-        alldata = []
-        for zone in ['IN', 'OUT']: # 0 = IN, 1 = OUT
-            zonedata = []
-            for subj in SUBJ_LIST:
-                subjdata = []
-                for bloc in BLOCS_LIST:
-                    mat = loadmat(PSDS_DIR + 'SA' + subj + '_' + bloc + '_' + zone + '_' + freq_name + '.mat')
-                    data = mat['PSD']
-                    if subjdata == []:
-                        subjdata = data
-                    else:
-                        subjdata = np.hstack((subjdata, data))
-                #subjdata = np.mean(subjdata, axis=1) # average blocs
-                if zonedata == []:
-                    zonedata = subjdata
-                else:
-                    zonedata = np.hstack((zonedata, subjdata))
-            alldata.append(np.asarray(zonedata)[:,:6500])
-        #alldata = np.array(alldata)
-        print(alldata[1].shape)
-        #0/0
-        #alldata_avg = np.mean(alldata, axis=1) # average subjects
-        alldata_avg = []
-        for alldat in alldata:
-            alldata_avg.append(np.mean(alldat, axis=1))
 
-        toplot.append((alldata_avg[0] - alldata_avg[1])/alldata_avg[1]) ### IN - OUT / OUT
-        tvals, pvals = ttest_perm(alldata[0].T, alldata[1].T, n_perm=1000, n_jobs=6)
+    # get PSDS averaged across trials (1 value per subj per sensor)
+    for i, freq in enumerate(FREQS_NAMES):
+        data_IN = []
+        data_OUT = []
+        for subj, _ in enumerate(SUBJ_LIST):
+            dat = PSD_alldata[i][0][subj]
+            data_IN.append(np.mean(dat, axis=1))
+            dat = PSD_alldata[i][1][subj]
+            data_OUT.append(np.mean(dat, axis=1))
 
-        tvalues.append(tvals)
-        pvalues.append(pvals)
-        print(pvals)
-        masks.append(create_pval_mask(pvals, alpha=0.1))
+        data_IN = np.array(data_IN)
+        data_OUT = np.array(data_OUT)
+        data_IN_avg = np.mean(data_IN, axis=0)
+        data_OUT_avg = np.mean(data_OUT, axis=0)
+        power_diff.append((data_IN_avg - data_OUT_avg)/data_OUT_avg) ### IN - OUT / OUT
+        #tvals, pvals = ttest_perm(alldata[0].T, alldata[1].T, n_perm=1000, n_jobs=6)
 
 
+        #tvalues.append(tvals)
 
+        #pvalues.append(pvals)
+        #print(pvals)
+        #masks.append(create_pval_mask(pvals, alpha=0.1))
 
-    ## Plot functions
-    # Create masks with pvalues
-
-    vmax = np.max(np.max(abs(np.asarray(tvalues))))
+    toplot = power_diff
+    vmax = np.max(np.max(abs(np.asarray(toplot))))
     vmin = -vmax
-    array_topoplot(tvalues, ch_xy, showtitle=True, titles=FREQS_NAMES, savefig=False, figpath=IMG_DIR + 'IN_vs_OUT_PSD_autoreject.png', vmin=vmin, vmax=vmax, with_mask=True, masks=masks)
+    array_topoplot(toplot, ch_xy, showtitle=True, titles=FREQS_NAMES, savefig=False, figpath=IMG_DIR + 'IN_vs_OUT_PSD_autoreject.png', vmin=vmin, vmax=vmax)#, with_mask=True, masks=masks)
--- scripts/saflow_preproc.py
+++ scripts/saflow_preproc.py
@@ -28,8 +28,9 @@ except:
 
 
 
-subj_list = ['04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14']
-blocs_list = ['0', '1', '2', '3', '4', '5', '6', '7']
+SUBJ_LIST = ['04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15']
+BLOCS_LIST = ['0', '1', '2', '3', '4', '5', '6', '7']
+BLOCS_TASK = ['1', '2', '3', '4', '5', '6']
 all_files = os.listdir(DATAPATH)
 
 
@@ -46,87 +47,69 @@ def find_rawfile(subj, bloc, all_files):
 
 def saflow_preproc(filepath, savepath, reportpath):
     report = mne.Report(verbose=True)
-
     raw_data = read_raw_ctf(filepath, preload=True)
     picks = mne.pick_types(raw_data.info, meg=True, eog=True, exclude='bads')
-
     fig = raw_data.plot(show=False);
     report.add_figs_to_section(fig, captions='Time series', section='Raw data')
     closefig(fig)
-
     fig = raw_data.plot_psd(average=False, picks=picks, show=False);
     report.add_figs_to_section(fig, captions='PSD', section='Raw data')
     closefig(fig)
-
-
     ## Filtering
     high_cutoff = 200
     low_cutoff = 0.5
     raw_data.filter(low_cutoff, high_cutoff, fir_design="firwin")
     raw_data.notch_filter(np.arange(60, high_cutoff+1, 60), picks=picks, filter_length='auto',phase='zero', fir_design="firwin")
-
     fig = raw_data.plot_psd(average=False, picks=picks, fmax=120, show=False);
     report.add_figs_to_section(fig, captions='PSD', section='Filtered data')
     closefig(fig)
-
     ## ICA
     ica = ICA(n_components=20, random_state=0).fit(raw_data, decim=3)
     fig = ica.plot_sources(raw_data, show=False);
     report.add_figs_to_section(fig, captions='Independent Components', section='ICA')
     closefig(fig)
-    fmax = 40. ## correlation threshold for ICA components (maybe increase to 40. ?)
-
+    fmax = 0.40 ## correlation threshold for ICA components (maybe increase to 40. ?)
     ## FIND ECG COMPONENTS
     ecg_epochs = create_ecg_epochs(raw_data, ch_name='EEG059')
-    ecg_inds, ecg_scores = ica.find_bads_ecg(ecg_epochs, ch_name='EEG059')
-
+    ecg_inds, ecg_scores = ica.find_bads_ecg(ecg_epochs, ch_name='EEG059', method='ctps', threshold=fmax)
     fig = ica.plot_scores(ecg_scores, ecg_inds, show=False);
     report.add_figs_to_section(fig, captions='Correlation with ECG (EEG059)', section='ICA - ECG')
     closefig(fig)
-
     fig = list()
     fig = ica.plot_properties(ecg_epochs, picks=ecg_inds, psd_args={'fmax': fmax}, image_args={'sigma': 1.}, show=False);
     for i, figure in enumerate(fig):
         report.add_figs_to_section(figure, captions='Detected component ' + str(i), section='ICA - ECG')
         closefig(figure)
-
-
     ## FIND EOG COMPONENTS
     eog_epochs = create_eog_epochs(raw_data, ch_name='EEG057')
-    eog_inds, eog_scores = ica.find_bads_eog(eog_epochs, ch_name='EEG057')
+    eog_inds, eog_scores = ica.find_bads_eog(eog_epochs, ch_name='EEG057', method='ctps', threshold=fmax)
     fig = ica.plot_scores(eog_scores, eog_inds, show=False);
     report.add_figs_to_section(fig, captions='Correlation with EOG (EEG057)', section='ICA - EOG')
     closefig(fig)
-
     fig = list()
     fig = ica.plot_properties(eog_epochs, picks=eog_inds, psd_args={'fmax': fmax}, image_args={'sigma': 1.}, show=False);
     for i, figure in enumerate(fig):
         report.add_figs_to_section(figure, captions='Detected component ' + str(i), section='ICA - EOG')
         closefig(figure)
-
     ## EXCLUDE COMPONENTS
     ica.exclude = ecg_inds
     ica.apply(raw_data)
     ica.exclude = eog_inds
     ica.apply(raw_data)
-
     fig = raw_data.plot(show=False); # Plot the clean signal.
     report.add_figs_to_section(fig, captions='After filtering + ICA', section='Raw data')
     closefig(fig)
-
     ## SAVE PREPROCESSED FILE
-    report.save(reportpath)
+    report.save(reportpath);
     try:
         raw_data.save(savepath, overwrite=False)
     except:
         print('File already exists')
 
 
-
-
-for subj in subj_list:
-    for bloc in blocs_list:
+for subj in SUBJ_LIST:
+    for bloc in BLOCS_LIST:
         file = find_rawfile(subj, bloc, all_files)
         save_pattern = PREPROCPATH + 'SA' + subj + '_' + bloc + '_preproc_raw.fif.gz'
         report_pattern = REPORTPATH + 'SA' + subj + '_' + bloc + '_preproc_report.html'
-        saflow_preproc(DATAPATH + file, save_pattern, report_pattern)
+		saflow_preproc(DATAPATH + file, save_pattern, report_pattern)

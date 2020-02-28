c3608355e198127248e987864d20b455fa9b493e
hyruuk lab <yharel109@gmail.com>
2020-02-28 11:05:16 -0500

    added stage arg to splitTrials

--- scripts/saflow_splitTrials.py
+++ scripts/saflow_splitTrials.py
@@ -5,7 +5,7 @@ from scipy.io import savemat
 import pickle
 
 if __name__ == "__main__":
-    PSD_alldata = split_PSD_data(FOLDERPATH, SUBJ_LIST, BLOCS_LIST, by='VTC', lobound=0.15, hibound=0.85)
+    PSD_alldata = split_PSD_data(FOLDERPATH, SUBJ_LIST, BLOCS_LIST, by='VTC', lobound=0.15, hibound=0.85, stage='PSD')
     PSD_dict = {'PSD_alldata': PSD_alldata}
 
     with open(FEAT_PATH + 'PSD_VTC1585', 'wb') as fp:

37b9248636c1e69427a830f8029533fda8333dbc
Hyruuk <yharel109@gmail.com>
2021-01-16 22:38:58 -0500

    try for loop in slurm submission

--- scripts/submission_connectivity.sh
+++ scripts/submission_connectivity.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #SBATCH --account=def-kjerbi
-#SBATCH --mem=512G
-#SBATCH --time=12:00:00
-#SBATCH --nodes=2
-#SBATCH --job-name=saflow_con
-#SBATCH --ntasks-per-node=24
+
 
 # Load the module:
 
@@ -16,9 +12,19 @@ module load python/3.8.0
 export SUBJECTS_DIR=/projects/def-kjerbi/hyruuk/saflow/saflow_anat
 source $EBROOTFREESURFER/FreeSurferEnv.sh
 
-echo "Starting run at: `date`"
 
-$SUBID = "$1"
-$HOME/neuropycon/bin/python $HOME/projects/def-kjerbi/hyruuk/saflow/scripts/saflow_connectivity.py $SUBID
+for i in 04 05 06 07 08 09 10 11 12 13 14; do
+  srun \
+    -N1 \
+    --mem=512G \
+    --ntasks-per-node=24 \
+    --nodes=2 \
+    --time=12:00:00 \
+    --job-name=saflow_con \
+echo "Starting run at: `date`"
+$HOME/neuropycon/bin/python $HOME/projects/def-kjerbi/hyruuk/saflow/scripts/saflow_connectivity.py $i
 
 echo "Program finished with exit code $? at: `date`"
+done
+
+wait
